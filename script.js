18b01bb2960e8025ab3e86e9488ef69cf9798a04
Jaime Rueda <jaime_rueda10@hotmail.com>
2020-01-03 13:41:07 -0600

    playing unit testing with jest

---
+++ script.js
@@ -0,0 +1,20 @@
+const googleDatabase = [
+    'cats.com',
+    'souprecipes.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavoritecats.com',
+    'myfavoritecats2.com'
+];
+
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter(website => {
+        return website.includes(searchInput);
+    })
+    return matches.length > 3 ? matches.slice(0, 3) : matches
+}
+
+// console.log(googleSearch('soup', googleDatabase));
+
+module.exports = googleSearch;
---
+++ script.test.js
@@ -0,0 +1,32 @@
+const googleSearch = require('./script');
+
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'dogpictures.com'
+];
+
+describe('googleSearch', () => {
+    
+    it('it is a silly test', () => {
+    expect('hello').toBe('hello')
+    googleSearch('testtest', dbMock)
+    })
+
+    it('is searching google', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual([])
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+    })
+
+    it('Work with undifined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    })
+
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    })
+    
+})
+
